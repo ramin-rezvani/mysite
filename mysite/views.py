a3cb1cd46147fbcd80b0502a14cd90cd3fbb14ce
ramin-rezvani <150137379+ramin-rezvani@users.noreply.github.com>
2023-11-09 23:11:45 +0330

    create json
    json

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 def http_test(request):
     return HttpResponse("<h1>thin is a test</h1>")
+
+
+def json_test(request):
+    return JsonResponse({"name":"ramin rezvani"})
+
