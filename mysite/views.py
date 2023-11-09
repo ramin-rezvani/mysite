b2642535f8a311e33923ac85d4289e7f0335e400
ramin-rezvani <150137379+ramin-rezvani@users.noreply.github.com>
2023-11-10 01:24:45 +0330

    create first app
    create first app named websit and tree views home and about and contact

--- mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-def http_test(request):
-    return HttpResponse("<h1>thin is a test</h1>")
-
-
-def json_test(request):
-    return JsonResponse({"name":"ramin rezvani"})
-
