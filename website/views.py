8562f348c10c3f303e4b16fe5b9bdd0274bc3895
ramin-rezvani <150137379+ramin-rezvani@users.noreply.github.com>
2023-11-10 22:16:45 +0330

    create render html
    create templates folder and index,about,contact html page

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(request):
-    return HttpResponse("<h1>Home page</h1>")
+    return render(request,"website/index.html")
 
 def about_view(request):
     return HttpResponse("<h1>About page</h1>")
